dfeaf78c30e72ce442678b66377d5dacb67e8af5
Anis <anis.rahman@nfq.com>
2023-06-13 11:08:58 +0700

    Wild card characters

---
+++ wildcard.sql
@@ -0,0 +1,12 @@
+-- RUN foreignKey.sql queries before
+
+-- Wild card characters % _
+-- used to substitue characters in string 
+
+-- % can be used for startsWith or endsWith
+SELECT * FROM customers WHERE customer_name LIKE "a%";
+SELECT * FROM customers WHERE customer_name LIKE "%w";
+
+-- _ can be used for contains or matching length
+SELECT * FROM customers WHERE customer_name LIKE "_ar_";
+SELECT * FROM customers WHERE customer_name LIKE "____";
